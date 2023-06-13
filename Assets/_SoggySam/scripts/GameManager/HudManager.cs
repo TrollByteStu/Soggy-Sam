e84a58753c41506138efd3b1120a125a46aa1240
infernerner <101186811+infernerner@users.noreply.github.com>
2023-06-13 13:56:42 +0200

    HudManager and health bar

---
+++ Assets/_SoggySam/scripts/GameManager/HudManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HudManager : MonoBehaviour
+{
+
+    public Transform _HealthBar;
+    public TMP_Text _HealthText;
+
+    public void FixedUpdate()
+    {
+        _HealthText.text = GameManager.Instance.stats._CurrentHealth + " / " + GameManager.Instance.stats._MaxHealth;
+        _HealthBar.transform.localScale =new (1f * (GameManager.Instance.stats._CurrentHealth / GameManager.Instance.stats._MaxHealth),1,1);
+    }
+}
