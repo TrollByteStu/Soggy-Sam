6e45e11f5bd4c73594a1daabae1e04bb4027a403
infernerner <101186811+infernerner@users.noreply.github.com>
2023-06-13 14:04:33 +0200

    gamemanager prfab update

--- Assets/_SoggySam/scripts/GameManager/GameManager.cs
+++ Assets/_SoggySam/scripts/GameManager/GameManager.cs
@@ -6,15 +6,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager _instance;
-    public MissionManager missionManager;
     public GameObject player;
     public playerStats stats;
 
     public void Awake()
     {
         _instance = this;
-        if (GetComponent<MissionManager>())
-            missionManager = GetComponent<MissionManager>();
         else
         {
             gameObject.AddComponent<MissionManager>();
