0e1ecea2be0a8d51e5548d04fed7a8e625e9532e
infernerner <101186811+infernerner@users.noreply.github.com>
2023-06-19 10:24:39 +0200

    moby dick 2 moves

--- Assets/_SoggySam/scripts/fish/boss scripts/moby dick/mobyDick.cs
+++ Assets/_SoggySam/scripts/fish/boss scripts/moby dick/mobyDick.cs
@@ -34,6 +34,7 @@ public class mobyDick : MonoBehaviour
                 if (_InWater) Charge();
                 break;
             case 2:
+                BellyFlop();
                 break;
         }
         
@@ -42,7 +43,7 @@ public class mobyDick : MonoBehaviour
     {
         if (_InWater) Move();
         if (_EndOfMoveTime + Random.Range(5f, 30f) <= Time.time)
-            _CurrentMove = Random.Range(1, 1);
+            _CurrentMove = Random.Range(1, 2);
 
 
     }
@@ -59,7 +60,7 @@ public class mobyDick : MonoBehaviour
     {
         if (_ChargeUp) // swim away
         {
-            transform.LookAt(_myPlayer.transform);
+            transform.LookAt(_myPlayer.transform.position + (Vector3.up * 10f));
             transform.Rotate(Vector3.up,180);
             _myRB.AddForce(2 * _Speed * Time.fixedDeltaTime * transform.forward);
         }
@@ -83,12 +84,46 @@ public class mobyDick : MonoBehaviour
 
     }
 
+    private void BellyFlop()
+    {
+        if (_ChargeUp && _InWater) // swim away
+        {
+            transform.LookAt(_myPlayer.transform.position + (Vector3.up * 70f));
+            transform.Rotate(Vector3.up, 180);
+            _myRB.AddForce(2 * _Speed * Time.fixedDeltaTime * transform.forward);
+        }
+        else if (!_ChargeUp && _InWater) // swim towards
+        {
+            transform.LookAt(_myPlayer.transform.position + (Vector3.up * 150f));
+            _myRB.AddForce(10 * _Speed * Time.fixedDeltaTime * transform.forward);
+        }
+
+        if (_myRB.velocity.y < 0 && !_InWater)
+        {
+            transform.LookAt(_myPlayer.transform);
+        }
+
+        if (100f <= Vector3.Distance(_myPlayer.transform.position, transform.position) && _ChargeUp) // swim 100 units away 
+        {
+            _ChargeUp = false;
+        }
+        else if (10f >= Vector3.Distance(_myPlayer.transform.position, transform.position) && !_ChargeUp) // end of move
+        {
+            _ChargeUp = true;
+            _myRB.AddForce(5 * _Speed * Time.fixedDeltaTime * transform.forward);
+            _CurrentMove = 0;
+            _EndOfMoveTime = Time.time;
+
+        }
+    }
+
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Water"))
         {
             _myRB.drag = 5;
+            _myRB.angularDrag = 2f;
             _myRB.useGravity = false;
             _InWater = true;
         }
@@ -98,7 +133,8 @@ public class mobyDick : MonoBehaviour
     {
         if (other.CompareTag("Water"))
         {
-            _myRB.drag = 1;
+            _myRB.drag = 0.5f;
+            _myRB.angularDrag = 0.5f;
             _myRB.useGravity = true;
             _InWater = false;
         }
