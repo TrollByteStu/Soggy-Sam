0e4e25f5179b1d9ff57ebbf327ea75646a77bb71
sayronaught <sayro_naught@hotmail.com>
2023-06-19 08:53:52 +0200

    and finally the script that makes it happen

---
+++ Assets/_SoggySam/scripts/ui/gameStatePopUp.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gameStatePopUp : MonoBehaviour
+{
+    // this canvas group add a simple and fade opacity fader
+    private CanvasGroup myUIgroup;
+
+    // the list of the UI objects we want to show
+    [SerializeField] private GameObject[] uiList;
+
+    // This hides all the objects, unhides the one we want and start the fade anim
+    public void showPopup(int i)
+    {
+        // make group visible
+        myUIgroup.alpha = 1;
+
+        // turn off all elements, this might be 20+ one day
+        if (uiList.Length > 0)
+        {
+            foreach(GameObject GO in uiList)
+            {
+                GO.SetActive(false);
+            }
+        }
+
+        // turn on the right one
+        uiList[i].SetActive(true);
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set canvas group
+        myUIgroup = GetComponent<CanvasGroup>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if we are fading
+        if ( myUIgroup.alpha > 0f)
+        {
+            // fade a bit
+            myUIgroup.alpha *= 0.99f;
+
+            // if we have faded a lot, set to zero
+            if (myUIgroup.alpha < 0.01f) myUIgroup.alpha = 0f;
+        }
+    }
+}
