08954e5ffaded6a66567d75c027e6db8cb886903
infernerner <101186811+infernerner@users.noreply.github.com>
2023-06-20 06:03:51 +0200

    menu and pause on escape

--- Assets/_SoggySam/scripts/GameManager/HudManager.cs
+++ Assets/_SoggySam/scripts/GameManager/HudManager.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.InputSystem;
 
 public class HudManager : MonoBehaviour
 {
 
     public Transform _HealthBar;
     public TMP_Text _HealthText;
+
+    public GameObject _Menu;
 
     public GameManager myGM;
     public gameStatePopUp myGameStateUI;
@@ -16,5 +19,21 @@ public class HudManager : MonoBehaviour
     {
         _HealthText.text = GameManager.Instance.stats._CurrentHealth + " / " + GameManager.Instance.stats._MaxHealth;
         _HealthBar.transform.localScale =new (1f * (GameManager.Instance.stats._CurrentHealth / GameManager.Instance.stats._MaxHealth),1,1);
+    }
+
+    void OnEscape(InputValue value)
+    {
+        if (_Menu.activeSelf == false)
+        {
+            _Menu.SetActive(true);
+            GameManager.Instance.player.GetComponent<PlayerInput>().enabled = false;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            _Menu.SetActive(false);
+            GameManager.Instance.player.GetComponent<PlayerInput>().enabled = true;
+            Time.timeScale = 1;
+        }
     }
 }
